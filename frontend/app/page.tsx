50628d6ab573c8dbd599fa49a122ce6aea504e88
Son Beomsoo <k1a11220@naver.com>
2024-05-18 18:36:23 -0400

    init

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="w-full h-full text-center">ChronoCraft</h1>
+    </main>
+  );
+}
