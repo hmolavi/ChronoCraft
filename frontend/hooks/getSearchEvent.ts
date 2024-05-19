1a349d7584cc37229098a4c878171b90feeab147
Abtin Abbaspour <34379699+abtin-abbaspour@users.noreply.github.com>
2024-05-19 00:10:45 -0400

    refactor: Improve readability of getSearchEvent query string

--- frontend/hooks/getSearchEvent.ts
+++ frontend/hooks/getSearchEvent.ts
@@ -42,7 +42,7 @@ async function getSearchEvent({ queryTopic, numEvents }: Props) {
   console.log("Calling the OpenAI API");
   const systemMessage =
     "You are an assistant that only writes JSON. Do not write normal text.";
-  const query = `Give me a list of ${numEvents} of the most significant historical event during ${queryTopic} formatted in the following way filling in the brackets\: \n{\nstart_date: {year: (start year), month: (start month), day: (start day),},\nmedia: { url: "", caption: "", link: "",},\nend_date: { year: (end year), month: (end month), day: (end day),},\n\n unique_id: "",\n text: { headline: "(title of the event)", text: "(concise summary of the event)",},\nbackground: {},},`;
+  const query = `Give me a list of ${numEvents} of the most significant historical event during ${queryTopic} formatted in the following way filling in the brackets: {start_date: {year: (start year), month: (start month), day: (start day),}, media: { url: "", caption: "", link: "",}, end_date: { year: (end year), month: (end month), day: (end day),}, unique_id: "", text: { headline: "(title of the event)", text: "(concise summary of the event)",}, background: {},},`;
 
   // For 0-10
   // What is the sentiment of this tweet with a value between 0 and 10 (10 being its very positive)? 
