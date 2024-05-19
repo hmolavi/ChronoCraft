0837d1dd818c4f9950c488dde6d32ed5072831d0
Son Beomsoo <k1a11220@naver.com>
2024-05-18 21:14:34 -0400

    Update style

--- frontend/components/SearchBar.tsx
+++ frontend/components/SearchBar.tsx
@@ -1,9 +1,12 @@
 export const SearchBar = () => {
   return (
     <div className="fixed bottom-0 w-full m-auto p-4 flex items-center justify-center">
-      <div className="bg-neutral-50 p-4 rounded-xl shadow-sm">
+      <div className="bg-neutral-50 border border-solid border-black/10 w-[400px] p-4 rounded-xl shadow-sm">
         {/* <p className="text-lg">When do you want to travel to?</p> */}
-        <input placeholder="Insert Query" className="bg-transparent" />
+        <input
+          placeholder="Search every history"
+          className="w-full bg-transparent outline-none text-black focus:border-none focus:ring-0"
+        />
         {/* <button>Browse</button> */}
       </div>
     </div>
