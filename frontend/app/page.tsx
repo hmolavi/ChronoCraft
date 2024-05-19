efb1c9e501fb7dcef14e11cb2d0fc71614d9b2bf
Son Beomsoo <k1a11220@naver.com>
2024-05-19 04:07:20 -0400

    Add loading

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -35,54 +35,11 @@ const Home = () => {
   const randomQuote = Quotes[Math.floor(Math.random() * Quotes.length)];
   const { empty, loading, timeline } = useTimelineStore();
 
-  const timelineString: string = JSON.stringify(timeline); // Convert events array to JSON string
-
-  // Use the timelineString variable instead of the timeline variable
-  const events: Slide[] = [
-    {
-      start_date: { year: 1919, month: 3, day: 1 },
-      media: { url: "", caption: "", link: "" },
-      end_date: { year: 1919, month: 4, day: 10 },
-      unique_id: "",
-      text: {
-        headline: "March 1st Movement",
-        text: "A nationwide protest against Japanese colonial rule in Korea, sparked by students and activists calling for independence.",
-      },
-      background: {},
-    },
-    {
-      start_date: { year: 1950, month: 6, day: 25 },
-      media: { url: "", caption: "", link: "" },
-      end_date: { year: 1953, month: 7, day: 27 },
-      unique_id: "",
-      text: {
-        headline: "Korean War",
-        text: "A war fought between North Korea, supported by China and the Soviet Union, and South Korea, supported by the United Nations and the United States.",
-      },
-      background: {},
-    },
-    {
-      start_date: { year: 1988, month: 9, day: 17 },
-      media: { url: "", caption: "", link: "" },
-      end_date: { year: 1988, month: 10, day: 2 },
-      unique_id: "",
-      text: {
-        headline: "Seoul Summer Olympics",
-        text: "The 1988 Summer Olympics held in Seoul, South Korea, which marked South Korea's rise as a major economic and cultural power.",
-      },
-      background: {},
-    },
-  ];
-
-  // console.log("timeline", JSON.parse(timeline));
-  console.log("events", events);
-  console.log("timelineString", timelineString);
-
   return (
     <div>
       <h1>My Timeline</h1>
       <SearchBar />
-      {empty && (
+      {/* {empty && !loading && (
         <div
           className="w-full gap-4 flex flex-col items-center justify-center"
           style={{ height: "calc(100vh - 80px)" }}
@@ -94,7 +51,32 @@ const Home = () => {
             {randomQuote.person}
           </p>
         </div>
-      )}
+      )} */}
+      {/* {loading && ( */}
+      <div
+        role="status"
+        className="w-full flex items-center justify-center"
+        style={{ height: "calc(100vh - 80px)" }}
+      >
+        <svg
+          aria-hidden="true"
+          className="inline w-10 h-10 text-gray-200 animate-spin  fill-blue-600"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+        <span className="sr-only">Loading...</span>
+      </div>
+      {/* )} */}
       {!empty && !loading && (
         <Timeline
           target={
