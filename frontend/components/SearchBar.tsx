d1ced2aeedb2469cc0952b152d273e2a87082c9f
M.Amin Mojtahed <amin.mojtahed@uwaterloo.ca>
2024-05-18 23:59:27 -0400

    fixed OPENAI API

--- frontend/components/SearchBar.tsx
+++ frontend/components/SearchBar.tsx
@@ -1,6 +1,8 @@
 import getSearchEvent from "@/hooks/getSearchEvent";
+import { useState } from "react";
 
 export const SearchBar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   return (
     <div className="fixed bottom-0 w-full m-auto p-4 flex items-center justify-center">
       <div className="bg-neutral-50 border flex border-solid border-black/10 w-[400px] p-2 rounded-2xl shadow-sm">
@@ -8,18 +10,21 @@ export const SearchBar = () => {
         <input
           placeholder="Search every history"
           className="w-full pl-2 bg-transparent outline-none text-black focus:border-none focus:ring-0"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
 
         <button
-          onClick={() =>
-            getSearchEvent("World War 1", 2)
+          onClick={() => {
+            console.log("Search Query:", searchQuery);
+            getSearchEvent({queryTopic: searchQuery, numEvents:3})
               .then((res) => {
                 console.log(res);
               })
               .catch((err) => {
                 console.error(err);
               })
-          }
+          }}
           className="bg-neutral-200 text-neutral-800 font-medium text-sm px-3 py-2 rounded-lg"
         >
           Search
